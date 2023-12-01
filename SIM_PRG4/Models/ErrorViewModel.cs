22e201878f25b3d3cc8e605bc63f076e8f0ae1ea
Branda <135972024+BranGallagher@users.noreply.github.com>
2023-12-01 22:25:46 +0700

    Add files via upload

---
+++ SIM_PRG4/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SIM_PRG4.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
